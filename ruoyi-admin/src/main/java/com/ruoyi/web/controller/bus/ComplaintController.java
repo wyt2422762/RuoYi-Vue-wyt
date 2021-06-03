5cc8d5172432a2e2651f6e6588e6192add387444
wyt2422762 <724752500@qq.com>
2021-06-03 16:21:10 +0800

    修改

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/bus/ComplaintController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/bus/ComplaintController.java
@@ -5,11 +5,7 @@ import java.util.List;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
@@ -52,7 +48,7 @@ public class ComplaintController extends BaseController {
     @GetMapping("/export")
     public AjaxResult export(Complaint complaint) {
         List<Complaint> list = complaintService.selectComplaintList(complaint);
-        ExcelUtil<Complaint> util = new ExcelUtil<Complaint>(Complaint.class);
+        ExcelUtil<Complaint> util = new ExcelUtil<>(Complaint.class);
         return util.exportExcel(list, "投诉数据");
     }
 
