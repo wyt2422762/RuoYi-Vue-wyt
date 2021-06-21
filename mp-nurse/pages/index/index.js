432a757584d70c991c843240fab12815cd424531
wyt2422762 <724752500@qq.com>
2021-06-21 19:55:04 +0800

    修改

--- mp-nurse/pages/index/index.js
+++ mp-nurse/pages/index/index.js
@@ -1,91 +1,47 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    markers: [{
-      iconPath: "/resources/others.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
-    }],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color:"#FF0000DD",
-      width: 2,
-      dottedLine: true
-    }],
-    controls: [{
-      id: 1,
-      iconPath: '../../img/1.3.png',
 
-      position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
-      },
-      clickable: true
-    }]
-  },
-  regionchange(e) {
-    console.log(e.type)
-  },
-  markertap(e) {
-    console.log(e.detail.markerId)
-  },
-  controltap(e) {
-    console.log(e.detail.controlId)
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        addmissage: '选的位置',
+        // markers	 Array	标记点
+        stitle: '故宫',
+        latitude: "",
+        longitude: "",
+        scale: 14,
+        markers: [],
+        //controls控件 是左下角圆圈小图标,用户无论放大多少,点这里可以立刻回到当前定位(控件（更新一下,即将废弃，建议使用 cover-view 代替）)
+        controls: [{
+            id: 1,
+            iconPath: '../../images/img/controls.png',
+            position: {
+                left: 15,
+                top: 260 - 50,
+                width: 40,
+                height: 40
+            },
+            clickable: true
+        }],
+        distanceArr: []
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onShow: function (options) {
+        let that = this
+        //获取当前的地理位置、速度
+        wx.getLocation({
+            type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+            success: function (res) {
+                //赋值经纬度
+                that.setData({
+                    latitude: res.latitude,
+                    longitude: res.longitude,
+
+                })
+            }
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
 })
