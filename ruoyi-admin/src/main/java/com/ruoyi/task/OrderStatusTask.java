4f3b223b95ca5ed638b400653651e0bf841ae761
wyt2422762 <724752500@qq.com>
2021-06-22 17:45:10 +0800

    修改

--- ruoyi-admin/src/main/java/com/ruoyi/task/OrderStatusTask.java
+++ ruoyi-admin/src/main/java/com/ruoyi/task/OrderStatusTask.java
@@ -3,6 +3,8 @@ package com.ruoyi.task;
 import com.ruoyi.bus.domain.Order;
 import com.ruoyi.bus.service.IOrderService;
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.enm.OrderStatusEnum;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,32 +16,33 @@ import java.util.List;
  * @author wyt
  */
 @Component("orderStatusTask")
+@Log
 public class OrderStatusTask {
 
     @Autowired
     private IOrderService orderService;
 
     public void exec() {
-        System.out.println("执行定时任务");
-        /*List<Order> orders = orderService.selectOrderList(null);
+        log.info("执行定时任务");
+
+        List<Order> orders = orderService.selectOrderList(null);
         //获取当前时间
         String date = DateUtils.getDate();
-
         for (Order order : orders) {
             //1. 判断订单状态
             String status = order.getStatus();
             //2. 获取服务时间
             String workTime = order.getWorkTime();
-            if ("2".equals(status)) {
+            if (OrderStatusEnum.YZF.equals(status)) {
                 //已支付改成服务中，已完成
                 if (workTime.contains(" - ")) {
                     String[] times = workTime.split(" - ");
                     if (date.compareTo(times[1]) > 0) {
                         //改成已完成
-                        order.setStatus("4");
+                        order.setStatus(OrderStatusEnum.YWC);
                     } else if (date.compareTo(times[0]) >= 0) {
                         //改成服务中
-                        order.setStatus("3");
+                        order.setStatus(OrderStatusEnum.FWZ);
                     }
                 } else {
                     if (date.compareTo(workTime) > 0) {
@@ -50,22 +53,22 @@ public class OrderStatusTask {
                         order.setStatus("3");
                     }
                 }
-            } else if ("3".equals(status)) {
+            } else if (OrderStatusEnum.FWZ.equals(status)) {
                 //服务中改成已完成
                 if (workTime.contains(" - ")) {
                     String[] times = workTime.split(" - ");
                     if (date.compareTo(times[1]) >= 0) {
                         //改成已完成
-                        order.setStatus("4");
+                        order.setStatus(OrderStatusEnum.YWC);
                     }
                 } else {
                     if (date.compareTo(workTime) > 0) {
                         //改成已完成
-                        order.setStatus("4");
+                        order.setStatus(OrderStatusEnum.YWC);
                     }
                 }
             }
-        }*/
+        }
     }
 
 }
